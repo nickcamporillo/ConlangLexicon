1499e4a111947d90efc091ee3ff231311a881fc2
nickcamporillo <cbor_spambloc@yahoo.com>
2019-10-21 00:06:51 -0400

    Initial checkin

---
+++ Utilities/GeneralUtils.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using IModels;
+using WTF.Core.FileSystem;
+using ns = Newtonsoft;
+
+namespace Utilities
+{
+    public class GeneralUtils
+    {
+        public static string SerializeToJson<T>(List<T> args) where T: IModel
+        {
+            string rslt = string.Empty;
+            rslt = ns.Json.JsonConvert.SerializeObject(args);
+            return rslt;
+        }
+    }
+}
