e1dac66ab9c3bce84b00e76181e48eed7e83fb48
nickcamporillo <cbor_spambloc@yahoo.com>
2021-12-12 12:38:18 -0500

    Future service to import data from excel

---
+++ Services/ExcelService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WTF.Core.OleDbExcel;
+using Models;
+
+namespace LexServices
+{
+    public class ExcelService
+    {
+        private const string EXCEL_DATASOURCE = @"C:\GitHub\ConlangLexicon\Data";
+
+        private readonly OleDbExcelUtility util = new OleDbExcelUtility();
+        
+        public List<LexiconRaw> ExcelData { get; set; }
+
+        public ExcelService()
+        {
+            ExcelData = new List<LexiconRaw>();
+
+            var cnx = util.GetExcelConnection(EXCEL_DATASOURCE);
+            var dataTable = util.GetDataTableFromFirstExcelWorksheet(EXCEL_DATASOURCE);
+            var reader = dataTable.CreateDataReader();
+
+
+
+
+
+            
+        }
+    }
+}
